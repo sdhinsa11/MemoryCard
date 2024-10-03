9761346b97af9e2cd5ada84d962bdc08df14c0c6
SDHINSA <sohanad@icloud.com>
2024-10-03 10:48:31 -0600

    Creating card structure

---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+import './Card.css'
+
+function Card() {
+// when you grab from API and yeah
+
+//  function for fetching API 
+    return (
+      <div className='singleCard'>
+
+        
+
+      </div>
+    )
+  }
+  
+  export default Card
+  
